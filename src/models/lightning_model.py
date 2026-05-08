53f91e74f404f62a27397ac619b287d9e27681de
Andras Palasti <andras.palasti@daalab.com>
2026-05-08 07:56:49 +0200

    focal with ramp up

--- src/models/lightning_model.py
+++ src/models/lightning_model.py
@@ -368,7 +368,7 @@ class SegmentationLightningModule(pl.LightningModule):
 
         if self.hparams.get("training", {}).get("lr_scheduler", True):
             scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-                optimizer, mode="max", factor=0.5, patience=15, threshold=0.01
+                optimizer, mode="max", factor=0.5, patience=30, threshold=0.005
             )
             return [optimizer], [
                 {
